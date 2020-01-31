f9b267e9de3400a17f79bab20fe621b4cd6f211f
Ginés Navarro <gines@dekalabs.com>
2020-01-31 13:29:16 +0100

    Set GMSMapView Delegate

--- GoogleMapsRoutes/ViewController.swift
+++ GoogleMapsRoutes/ViewController.swift
@@ -33,7 +33,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        googleRouteService.getRoute(from: "1wpQjG6z9Bp2o4q0vRahpuOzbTug&ll")
+        //googleRouteService.getRoute(from: "1wpQjG6z9Bp2o4q0vRahpuOzbTug&ll")
+        googleRouteService.getRoute(from: "1qaXkVYuBo-r0tk28f-u9VcF6wKM&hl")
     }
 }
 
@@ -45,10 +46,11 @@ extension ViewController: GoogleRouteServiceDelegate {
     func didRoute(data: Data) {
         let kmlParser = GMUKMLParser(data: data)
         kmlParser.parse()
-        
+                
         let renderer = GMUGeometryRenderer(map: mapView,
                                            geometries: kmlParser.placemarks,
-                                           styles: kmlParser.styles)
+                                           styles: kmlParser.styles,
+                                           styleMaps: kmlParser.styleMaps)
         renderer.render()
     }
 }
@@ -57,4 +59,33 @@ extension ViewController: GoogleRouteServiceDelegate {
 
 extension ViewController: GMSMapViewDelegate {
     
+    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
+        return false
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTap overlay: GMSOverlay) {
+        
+    }
+    
+    func mapView(_ mapView: GMSMapView, didTapPOIWithPlaceID placeID: String, name: String, location: CLLocationCoordinate2D) {
+        
+    }
+
+    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        let name = marker.description
+        let description = marker.snippet
+        return nil
+    }
+    
+    func mapView(_ mapView: GMSMapView, markerInfoContents marker: GMSMarker) -> UIView? {
+        return nil
+    }
+    
+    func mapView(_ mapView: GMSMapView, didCloseInfoWindowOf marker: GMSMarker) {
+        
+    }
 }
